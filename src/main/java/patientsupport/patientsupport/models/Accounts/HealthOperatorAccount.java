2da2045f2dbde916c0b7ca12e1b0bcb5939ef317
Julian Hurtado <48264229+yuliusmj@users.noreply.github.com>
2019-06-09 11:14:45 -0500

    Create class HealthOperatorAccount

--- src/main/java/patientsupport/patientsupport/models/Accounts/HealthOperatorAccount.java
+++ src/main/java/patientsupport/patientsupport/models/Accounts/HealthOperatorAccount.java
@@ -1,10 +1,39 @@
 package patientsupport.patientsupport.models.Accounts;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 @Table(name = "HealthOperatorAccounts")
 public class HealthOperatorAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotEmpty(message = "This field is required")
+    @Length(max = 100)
+    private String Description;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        this.Description = description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 
     
     
