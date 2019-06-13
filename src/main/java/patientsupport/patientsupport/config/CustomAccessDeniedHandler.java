0c5f91d04886ae1ab88ec9dfa8365821c1cd13c6
Julian Hurtado <48264229+yuliusmj@users.noreply.github.com>
2019-06-12 20:11:00 -0500

    update classes configuration

--- src/main/java/patientsupport/patientsupport/config/CustomAccessDeniedHandler.java
+++ src/main/java/patientsupport/patientsupport/config/CustomAccessDeniedHandler.java
@@ -24,7 +24,8 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
         Authentication auth = SecurityContextHolder.getContext()
             .getAuthentication();
         if (auth != null) {
-            LOG.warn("User: " + auth.getName() + " attempted to access the protected URL: " + request.getRequestURI());
+            LOG.warn("User: " + auth.getName() + " attempted to access the protected URL: " 
+            + request.getRequestURI());
         }
 
         response.sendRedirect(request.getContextPath() + "/access-denied");
